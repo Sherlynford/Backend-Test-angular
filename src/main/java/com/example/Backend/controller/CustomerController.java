de285c5ea08f198e8fd32d16b316722f27b16683
Sherlynford <bobowba2547@gmail.com>
2024-06-15 14:55:33 +0700

    Update CustomerController.java

--- src/main/java/com/example/Backend/controller/CustomerController.java
+++ src/main/java/com/example/Backend/controller/CustomerController.java
@@ -14,7 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
-@CrossOrigin("http://localhost:4200")
+@CrossOrigin("*")
 public class CustomerController {
     
     private final CustomerService customerService;
